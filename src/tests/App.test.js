463df3e01501edae2263ae0f73eaf4e640f94bcc
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-27 17:15:10 -0300

    mudando nome botao

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -21,9 +21,9 @@ test('testa se tem o planeta Alderaan e quando filtrado por nome "ta" o planeta 
   expect(screen.getByText('Tatooine')).toBeInTheDocument()
 });
 
-test('testa o filtro "maior que"', () => {
+test('testa o filtro "greater than"', () => {
   fireEvent.change(screen.getByTestId('column-filter'), {target: {value: 'diameter'}})
-  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'maior que'}})
+  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'greater than'}})
   const input = screen.getByTestId('value-filter')
   userEvent.type(input, '12500');
   userEvent.click(screen.getByTestId('button-filter'))
@@ -32,9 +32,9 @@ test('testa o filtro "maior que"', () => {
   expect(tr.length).toBe(3)
 });
 
-test('testa o filtro "menor que"', () => {
+test('testa o filtro "less than"', () => {
   fireEvent.change(screen.getByTestId('column-filter'), {target: {value: 'orbital_period'}})
-  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'menor que'}})
+  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'less than'}})
   const input = screen.getByTestId('value-filter')
   userEvent.type(input, '350');
   userEvent.click(screen.getByTestId('button-filter'))
@@ -43,9 +43,9 @@ test('testa o filtro "menor que"', () => {
   expect(tr.length).toBe(4)
 });
 
-test('testa o filtro "igual"', () => {
+test('testa o filtro "equal to"', () => {
   fireEvent.change(screen.getByTestId('column-filter'), {target: {value: 'rotation_period'}})
-  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'igual a'}})
+  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'equal to'}})
   const input = screen.getByTestId('value-filter')
   userEvent.type(input, '18');
   userEvent.click(screen.getByTestId('button-filter'))
@@ -57,28 +57,28 @@ test('testa o filtro "igual"', () => {
 test('testa o botão de deletar filtro e remover todos os filtros', () => {
   //filtro 1
   fireEvent.change(screen.getByTestId('column-filter'), {target: {value: 'population'}})
-  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'maior que'}})
+  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'greater than'}})
   const input = screen.getByTestId('value-filter')
   userEvent.type(input, '1000');
   userEvent.click(screen.getByTestId('button-filter'))
   //filtro 2
   fireEvent.change(screen.getByTestId('column-filter'), {target: {value: 'orbital_period'}})
-  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'menor que'}})
+  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'less than'}})
   userEvent.type(input, '400');
   userEvent.click(screen.getByTestId('button-filter'))
   //filtro 3
   fireEvent.change(screen.getByTestId('column-filter'), {target: {value: 'rotation_period'}})
-  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'igual a'}})
+  fireEvent.change(screen.getByTestId('comparison-filter'), {target: {value: 'equal to'}})
   userEvent.type(input, '24');
   userEvent.click(screen.getByTestId('button-filter'))
   expect(screen.getAllByRole('row').length).toBe(3)
   
-  const deleteButton = screen.getAllByText(/Deletar/i)
+  const deleteButton = screen.getAllByTestId(/DeleteIcon/i)
   expect(deleteButton.length).toBe(3)
   userEvent.click(deleteButton[2])
   expect(screen.getAllByRole('row').length).toBe(5)
 
-  const buttonRemoveAllFilters = screen.getByText(/Remover/)
+  const buttonRemoveAllFilters = screen.getByText(/Remove All Filters/)
   userEvent.click(buttonRemoveAllFilters)
   expect(screen.getAllByRole('row').length).toBe(11)
 
@@ -88,7 +88,7 @@ test('testa ordenar de forma "ascendente"', () => {
   fireEvent.change(screen.getByTestId("column-sort"), {target: {value: 'population'}})
   const asc = screen.getByTestId('column-sort-input-asc')
   userEvent.click(asc)
-  userEvent.click(screen.getByText(/Ordenar/))
+  userEvent.click(screen.getByText(/SORT/))
   const tr = screen.getAllByRole('row')
   expect(tr[1]).toHaveTextContent(/Yavin/)
 });
@@ -97,7 +97,7 @@ test('testa ordenar de forma "descendente"', () => {
   fireEvent.change(screen.getByTestId("column-sort"), {target: {value: 'population'}})
   const asc = screen.getByTestId("column-sort-input-desc")
   userEvent.click(asc)
-  userEvent.click(screen.getByText(/Ordenar/))
+  userEvent.click(screen.getByText(/SORT/))
   const tr = screen.getAllByRole('row')
   expect(tr[1]).toHaveTextContent(/Coruscant/)
 });
