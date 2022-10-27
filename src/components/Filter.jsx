61c1a1659c8c9bbc3a0ba97f05347f3fc7dc0a3a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-27 17:11:58 -0300

    acrescentando um componente

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,51 +1,13 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
+import TextField from '@mui/material/TextField';
+import DeleteIcon from '@mui/icons-material/Delete';
 import starWarsContext from '../context/starWarsContext';
+import Order from './Order';
+import FilterByNumber from './FilterByNumber';
 
 function Filter() {
-  const { setPlanetList, setSearch,
-    retornoApi } = useContext(starWarsContext);
-  const [arrayOfFilters, setArrayOfFilters] = useState([]);
-  const [filter, setFilter] = useState(
-    { column: 'population',
-      comparison: 'maior que',
-      value: '0' },
-  );
-  const [arrayColumn, setArrayColumn] = useState(['population', 'orbital_period',
-    'diameter', 'rotation_period', 'surface_water']);
-
-  useEffect(() => {
-    setFilter({ column: arrayColumn[0],
-      comparison: 'maior que',
-      value: '0' });
-  }, [arrayColumn]);
-
-  const filterByValue = (param) => {
-    const { column, value, comparison } = param;
-    switch (comparison) {
-    case 'maior que':
-      setPlanetList((prevState) => prevState
-        .filter((planet) => planet[column] > Number(value)));
-      break;
-    case 'menor que':
-      setPlanetList((prevState) => prevState
-        .filter((planet) => planet[column] < Number(value)));
-      break;
-    default:
-      setPlanetList((prevState) => prevState
-        .filter((planet) => Number(planet[column]) === Number(value)));
-    }
-  };
-
-  const filters = () => {
-    setArrayColumn(arrayColumn.filter((item) => item !== filter.column));
-
-    const newArray = arrayOfFilters.concat(filter);
-    setArrayOfFilters(newArray);
-  };
-
-  useEffect(() => {
-    arrayOfFilters.forEach((param) => filterByValue(param));
-  }, [arrayOfFilters]);
+  const { setPlanetList, setSearch, arrayColumn, setArrayColumn,
+    retornoApi, arrayOfFilters, setArrayOfFilters } = useContext(starWarsContext);
 
   const removeFilter = (column) => {
     setPlanetList(retornoApi);
@@ -54,78 +16,28 @@ function Filter() {
     setArrayOfFilters(test);
   };
 
-  const removeAllFilters = () => {
-    setPlanetList(retornoApi);
-    setArrayColumn((['population', 'orbital_period',
-      'diameter', 'rotation_period', 'surface_water']));
-    setArrayOfFilters([]);
-  };
-
   return (
-    <div>
-      <input
+    <div className="filter">
+      <TextField
+        label="Planet Name"
         type="text"
         data-testid="name-filter"
         onChange={ ({ target }) => setSearch(target.value) }
       />
-      <select
-        data-testid="column-filter"
-        value={ filter.column }
-        onChange={ ({ target }) => setFilter({ ...filter, column: target.value }) }
-      >
-        {
-          arrayColumn.map((column, index) => (
-            <option key={ index } value={ column }>{column}</option>
-          ))
-        }
-      </select>
-      <select
-        data-testid="comparison-filter"
-        value={ filter.comparison }
-        onChange={ ({ target }) => setFilter({ ...filter, comparison: target.value }) }
-      >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        type="number"
-        data-testid="value-filter"
-        value={ filter.value }
-        onChange={ ({ target }) => setFilter({ ...filter, value: target.value }) }
-      />
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ filters }
-      >
-        {' '}
-        Filtrar
-
-      </button>
+      <div className="filterNumberAndOrder">
+        <FilterByNumber />
+        <Order />
+      </div>
       {
         arrayOfFilters.map((element, index) => (
-          <div data-testid="filter" key={ index }>
-            <span>{element.column}</span>
-            <span>{element.comparison}</span>
-            <span>{element.value}</span>
-            <button
-              type="button"
+          <div data-testid="filter" className="usedFilter" key={ index }>
+            <span>{`${element.column} ${element.comparison} ${element.value}`}</span>
+            <DeleteIcon
               onClick={ () => removeFilter(element.column) }
-            >
-              Deletar
-
-            </button>
+            />
           </div>
         ))
       }
-      <button
-        type="button"
-        data-testid="button-remove-filters"
-        onClick={ removeAllFilters }
-      >
-        Remover todas filtragens
-      </button>
     </div>
   );
 }
