c1ecdf7a9e64996bdac4bdbe23f60cdf97a8dd5a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-10-27 17:17:02 -0300

    add reset css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style/reset.scss';
+import './style/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
